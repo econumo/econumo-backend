6f91a03543e946a66f194c3b2948db22e68c78f9
Dmitry Kuznetsov <d.kuznetsov@skyeng.ru>
2021-11-30 23:26:40 +0300

    fix

--- tests/Helper/Api.php
+++ tests/Helper/Api.php
@@ -4,11 +4,7 @@ namespace App\Tests\Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-use App\Domain\Entity\ValueObject\Email;
 use App\Domain\Entity\ValueObject\Id;
-use App\Domain\Repository\UserRepositoryInterface;
-use App\Tests\ApiTester;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Ramsey\Uuid\Uuid;
 
 class Api extends \Codeception\Module
