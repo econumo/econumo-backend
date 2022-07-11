eb8623cf653feb76e981b6de23d31d12be797d08
Dmitry Kuznetsov <d.kuznetsov@skyeng.ru>
2022-07-11 20:13:25 +0300

    [update] added controller for load testing

---
+++ src/UI/Controller/LoadTestingController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoadTestingController extends AbstractController
+{
+    /**
+     * Check controller
+     *
+     * @Route("/_/load-testing", methods={"POST"})
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function __invoke(Request $request): Response
+    {
+        sleep(1);
+        return new JsonResponse(['success' => true]);
+    }
+}
