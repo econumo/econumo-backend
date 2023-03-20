b69106a11236595aff5206235879f9d03067a25e
Dmitry Kuznetsov <d.kuznetsov@skyeng.ru>
2023-03-19 23:55:33 -0700

    New relic installation

--- build/docker/app/configs/usr/production.sh
+++ build/docker/app/configs/usr/production.sh
@@ -5,13 +5,11 @@ echo "* * * * * www-data  cd /var/www && php bin/console schedule:run >> /dev/nu
 
 if [[ -n "${NEW_RELIC_API_KEY}" ]]; then
   mkdir -p /opt/newrelic && \
-      cd /opt/newrelic && \
       wget -qO- https://download.newrelic.com/php_agent/archive/10.7.0.319/newrelic-php5-10.7.0.319-linux.tar.gz | tar xvz -C /opt/newrelic && \
       cd /opt/newrelic/newrelic-php5-10.7.0.319-linux/ && \
       export NR_INSTALL_USE_CP_NOT_LN=1 && \
       export NR_INSTALL_SILENT=1 && \
-      source newrelic-install install && \
-      rm -rf /opt/newrelic
+      source newrelic-install install -n logs-integration
 fi
 
 /usr/bin/supervisord -n -c /etc/supervisord.conf
