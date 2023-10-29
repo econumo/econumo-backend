914fae5a43d569ca80591c13f6cdc43a82f2c4d5
Dmitry Kuznetsov <d.kuznetsov@skyeng.ru>
2023-10-29 09:58:17 -0700

    Fix tests

--- tests/api/v1/budget/TransferEnvelopePlanCest.php
+++ tests/api/v1/budget/TransferEnvelopePlanCest.php
@@ -18,7 +18,12 @@ class TransferEnvelopePlanCest
     public function requestShouldReturn200ResponseCode(ApiTester $I): void
     {
         $I->amAuthenticatedAsJohn();
-        $I->sendPOST($this->url, ['id' => 'test']);
+        $I->sendPOST($this->url, [
+            'fromEnvelopeId' => '10d26248-9518-46eb-84ca-6ab8d4cbcdbb',
+            'toEnvelopeId' => 'acb421b0-ea48-4efe-ab04-7bda5a5c68ae',
+            'period' => '2020-01-01 00:00:00',
+            'amount' => 100.0
+        ]);
         $I->seeResponseCodeIs(HttpCode::OK);
     }
 
@@ -37,7 +42,12 @@ class TransferEnvelopePlanCest
      */
     public function requestShouldReturn401ResponseCode(ApiTester $I): void
     {
-        $I->sendPOST($this->url, ['id' => 'test']);
+        $I->sendPOST($this->url, [
+            'fromEnvelopeId' => '10d26248-9518-46eb-84ca-6ab8d4cbcdbb',
+            'toEnvelopeId' => 'acb421b0-ea48-4efe-ab04-7bda5a5c68ae',
+            'period' => '2020-01-01 00:00:00',
+            'amount' => 100.0
+        ]);
         $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
     }
 
@@ -47,11 +57,12 @@ class TransferEnvelopePlanCest
     public function requestShouldReturnResponseWithCorrectStructure(ApiTester $I): void
     {
         $I->amAuthenticatedAsJohn();
-        $I->sendPOST($this->url, ['id' => 'test']);
-        $I->seeResponseMatchesJsonType([
-            'data' => [
-                'result' => 'string',
-            ],
+        $I->sendPOST($this->url, [
+            'fromEnvelopeId' => '10d26248-9518-46eb-84ca-6ab8d4cbcdbb',
+            'toEnvelopeId' => 'acb421b0-ea48-4efe-ab04-7bda5a5c68ae',
+            'period' => '2020-01-01 00:00:00',
+            'amount' => 100.0
         ]);
+        $I->seeResponseMatchesJsonType(['data' => [],]);
     }
 }
