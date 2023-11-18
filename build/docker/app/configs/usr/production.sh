b961ccc1c36f0699aa69dbf84f2fa656f41c6a1f
Dmitry Kuznetsov <d.kuznetsov@skyeng.ru>
2023-11-18 15:28:24 -0800

    Enable new relic for production

--- build/docker/app/configs/usr/production.sh
+++ build/docker/app/configs/usr/production.sh
@@ -3,18 +3,17 @@
 cd /var/www && php bin/console doctrine:migrations:migrate --quiet --no-interaction --allow-no-migration
 
 if [[ -n "${NEW_RELIC_API_KEY}" ]]; then
-  wget -qO- https://download.newrelic.com/php_agent/archive/10.7.0.319/newrelic-php5-10.7.0.319-linux.tar.gz | tar xvz -C /tmp && \
-      cd /tmp/newrelic-php5-*/ && \
-      export NR_INSTALL_USE_CP_NOT_LN=1 && \
-      export NR_INSTALL_SILENT=1 && \
-      sh newrelic-install install -n logs-integration && \
-      rm -rf /tmp/newrelic-php5-* /tmp/nrinstall* && \
-      sed -i \
-        -e "s/\"REPLACE_WITH_REAL_KEY\"/\"$NEW_RELIC_LICENSE_KEY\"/" \
-        -e "s/\"PHP Application\"/\"$NEW_RELIC_APP_NAME\"/" \
-        -e 's/;newrelic.daemon.app_connect_timeout =.*/newrelic.daemon.app_connect_timeout=15s/' \
-        -e 's/;newrelic.daemon.start_timeout =.*/newrelic.daemon.start_timeout=5s/' \
-        /usr/local/etc/php/conf.d/newrelic.ini
+  curl -L https://download.newrelic.com/php_agent/archive/${NEW_RELIC_AGENT_VERSION}/newrelic-php5-${NEW_RELIC_AGENT_VERSION}-linux.tar.gz | tar -C /tmp -zx \
+      && export NR_INSTALL_USE_CP_NOT_LN=1 \
+      && export NR_INSTALL_SILENT=1 \
+      && /tmp/newrelic-php5-${NEW_RELIC_AGENT_VERSION}-linux/newrelic-install install \
+      && rm -rf /tmp/newrelic-php5-* /tmp/nrinstall*
+
+  find /etc /opt /usr/local/etc -type f -name newrelic.ini \
+      -exec sed -i \
+          -e "s/REPLACE_WITH_REAL_KEY/${NEW_RELIC_LICENSE_KEY}/" \
+          -e "s/newrelic.appname[[:space:]]=[[:space:]].*/newrelic.appname = \"${NEW_RELIC_APPNAME}\"/" \
+          -e "s/newrelic.daemon.address[[:space:]]=[[:space:]].*/newrelic.daemon.address = \"${NEW_RELIC_AGENT_HOST}:31339\"/" {} \;
 fi
 
 /usr/bin/supervisord -n -c /etc/supervisord.conf
