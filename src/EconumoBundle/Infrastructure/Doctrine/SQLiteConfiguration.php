4abad2319f671f74042bb9adb13d2fca438c4b14
Dmitry <kuznetsov2d@gmail.com>
2024-12-20 22:20:42 -0800

    Add SQLite busy timeout configuration and rename related class.

--- src/EconumoBundle/Infrastructure/Doctrine/SQLiteConfiguration.php
+++ src/EconumoBundle/Infrastructure/Doctrine/SQLiteConfiguration.php
@@ -7,13 +7,20 @@ namespace App\EconumoBundle\Infrastructure\Doctrine;
 use Doctrine\DBAL\Event\ConnectionEventArgs;
 use Doctrine\DBAL\Platforms\SqlitePlatform;
 
-class SQLiteForeignKeyEnabler
+readonly class SQLiteConfiguration
 {
+    public function __construct(private int $busyTimeout = 0)
+    {
+    }
+
     public function postConnect(ConnectionEventArgs $args): void
     {
         $connection = $args->getConnection();
         if ($connection->getDatabasePlatform() instanceof SqlitePlatform) {
             $connection->executeStatement('PRAGMA foreign_keys = ON;');
+            if ($this->busyTimeout > 0) {
+                $connection->executeStatement(sprintf('PRAGMA busy_timeout = %d;', $this->busyTimeout));
+            }
         }
     }
 }
